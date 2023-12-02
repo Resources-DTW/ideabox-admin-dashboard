1be93d7fcfbd859704c1abc51195beee00bba86c
Resources-DTW <resource.dtw@gmail.com>
2023-12-02 16:03:01 +0530

    2/12 final commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Pie } from "@ant-design/plots";
 import {
   Button,
@@ -7,11 +7,20 @@ import {
   Menu,
   Card,
   Statistic,
-  Divider,
   Space,
+  Typography,
 } from "antd";
+import { PiCoinsThin } from "react-icons/pi";
+import { TbTruckDelivery } from "react-icons/tb";
+import { BsBox2 } from "react-icons/bs";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { MdOutlineHomeRepairService } from "react-icons/md";
 
 export default function Dashboard() {
+  const [loading, setLoading] = useState(false);
+  const [customerCount, setCustomerCount] = useState([]);
+  const [providersCount, SetProvidersCount] = useState([]);
+
   const data = [
     {
       type: "Active",
@@ -62,17 +71,63 @@ export default function Dashboard() {
   };
 
   return (
-    <div
-      style={{
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>
-        <Pie {...config} />
+    <>
+      <Space
+        direction="vertical"
+        style={{
+          alignItems: "center",
+          justifyContent: "center",
+          width: "100%",
+        }}
+      >
+        <Space
+          direction="horizontal"
+          style={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <DashboardCard title={"Total Materials"} count={120} />
+          <DashboardCard title={"Total Patients"} count={2120} />
+          <DashboardCard title={"Total Clinicians"} count={1120} />
+        </Space>
+        <Space
+          direction="horizontal"
+          style={{
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "100%",
+          }}
+        >
+          <DashboardCard title={"Subscribed User"} count={120} />
+          <DashboardCard title={"Guest Users"} count={120} />
+          <DashboardCard title={"Clinicians"} count={120} />
+        </Space>
+      </Space>
+      <div
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Pie {...config} />
+        </div>
+        <div>{/* <Line {...lineConfig} /> */}</div>
       </div>
-      <div>{/* <Line {...lineConfig} /> */}</div>
-    </div>
+    </>
   );
+
+  function DashboardCard({ title, count, icon }) {
+    return (
+      <Card>
+        <Space direction="horizontal">
+          {icon}
+          <Statistic title={title} value={count} />
+        </Space>
+      </Card>
+    );
+  }
 }
